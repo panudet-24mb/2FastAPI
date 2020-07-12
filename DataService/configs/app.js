66888cfe157b5d607152231b60f74d3cbff64b60
Panudet Panumas <panudet.panumas@gmail.com>
2020-07-12 19:33:08 +0700

    change HOST

--- DataService/configs/app.js
+++ DataService/configs/app.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 module.exports = {
     port: process.env.PORT || 3000,
-    hostaddress: process.env.HOST || 'localhost',
+    hostaddress: process.env.HOST || '2fast.online',
     isProduction: process.env.NODE_ENV === 'production',
     apiVersion: process.env.API_VERSION || 1,
     token_exp_days: process.env.TOKEN_EXP_DAYS || 1,
