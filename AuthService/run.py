f8027ac11b5460a34962553c097f95ed27623add
Panudet Panumas <47716860+panudet-24mb@users.noreply.github.com>
2020-07-14 16:47:00 +0700

    adhoc

--- AuthService/run.py
+++ AuthService/run.py
@@ -2,10 +2,6 @@ from app import app
 
 app.env = "development"
 app.run(
-    host="0.0.0.0",
-    port=5001,
-    threaded=True,
-    debug=False,
-    ssl_context=("/home/ubuntu/cert.pem", "/home/ubuntu/key.pem"),
+    host="0.0.0.0", port=5001, threaded=True, debug=False, ssl_context=("adhoc"),
 )
 
