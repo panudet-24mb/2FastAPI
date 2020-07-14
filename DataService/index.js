1c52a2915556aef9a799915746f433e867b9be74
Panudet Panumas <47716860+panudet-24mb@users.noreply.github.com>
2020-07-14 17:58:34 +0700

    SSL

--- DataService/index.js
+++ DataService/index.js
@@ -1,6 +1,8 @@
 const express = require('express'),
-      app = express(),
-      config = require('./configs/app')
+  https = require('https'),
+  app = express(),
+  config = require('./configs/app'),
+  fs = require('fs')
 
 // Express Configs
 require('./configs/express')(app)
@@ -14,9 +16,30 @@ app.use(require('./routes'))
 // Error handler
 require('./configs/errorHandler')(config.isProduction, app)
 
+
+
 // Start Server
-const server = app.listen( config.port, () => {
+// const server = app.listen(config.port, () => {
+//   let host = server.address().address
+//   let port = server.address().port
+//   console.log(`Server is running at http://${host}:${port}`)
+// })
+
+
+
+var https_options = {
+  key: fs.readFileSync('/home/ubuntu/key.pem'),
+  cert: fs.readFileSync('/home/ubuntu/cert.pem'),
+};
+// let host = server.address().address
+// let port = server.address().port
+
+
+const server = app.listen(config.port, () => {
+  https_options
   let host = server.address().address
   let port = server.address().port
   console.log(`Server is running at http://${host}:${port}`)
 })
+
+
