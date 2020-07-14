71d7913638a280b39ec5eea75d8e0144816951e8
Panudet Panumas <47716860+panudet-24mb@users.noreply.github.com>
2020-07-14 16:41:19 +0700

    SSL

--- AuthService/app/__init__.py
+++ AuthService/app/__init__.py
@@ -6,16 +6,16 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] =  'mysql://admin:Passw0rd_2020@localhost/2fast'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-Secret_key = 'thisissecret'
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://admin:Passw0rd_2020@localhost/2fast"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+Secret_key = "thisissecret"
 
-db = SQLAlchemy(
-    engine_options={ 'connect_args': { 'connect_timeout': 5 }}
-)
+
+db = SQLAlchemy(engine_options={"connect_args": {"connect_timeout": 5}})
 db.init_app(app)
 ma = Marshmallow(app)
 
 
 from app.Auth.views import AuthService
+
 app.register_blueprint(AuthService)
