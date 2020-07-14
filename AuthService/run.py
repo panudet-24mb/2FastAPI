bb8bde46b15d803adf08a69f658787c881a806e4
Panudet Panumas <47716860+panudet-24mb@users.noreply.github.com>
2020-07-14 17:14:07 +0700

    ยำท

--- AuthService/run.py
+++ AuthService/run.py
@@ -2,6 +2,10 @@ from app import app
 
 app.env = "development"
 app.run(
-    host="0.0.0.0", port=5001, threaded=True, debug=False, ssl_context=("adhoc"),
+    host="0.0.0.0",
+    port=5001,
+    threaded=True,
+    debug=False,
+    ssl_context=("/home/ubuntu/cert.pem", "/home/ubuntu/key.pem"),
 )
 
